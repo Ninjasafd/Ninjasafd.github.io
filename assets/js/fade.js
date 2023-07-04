a60d6ae18200b0916b1736d02b3dc904c753781e
Ninjasafd <74614776+Ninjasafd@users.noreply.github.com>
2023-07-04 16:29:41 -0500

    More animations

---
+++ assets/js/fade.js
@@ -0,0 +1,27 @@
+$(function () {  // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+});
+
+$(document).ready(function () {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.fade').each(function (i) {
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object - $(this).outerHeight() * 4 / 5) {
+
+                $(this).animate({ 'opacity': '1' }, 1000);
+
+            }
+
+        });
+
+    });
+
+});

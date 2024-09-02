2b9368c6f8d24e4add09088097ceb95bc093f54f
Ninjasafd <JeffreyPan95@gmail.com>
2024-09-02 02:46:30 -0400

    Header updates

--- my-portfolio/src/components/Header.tsx
+++ my-portfolio/src/components/Header.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { MailIcon } from '@heroicons/react/solid';
+import { faGithub, faLinkedin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <div className="flex flex-row items-center">
         <a href="https://github.com/Ninjasafd" aria-label="GitHub" className="text-gray-300 hover:text-white">
-          <FontAwesomeIcon icon={faGithub} size="2x" className="text-white" />
+          <FontAwesomeIcon icon={faGithub} size="lg" className="text-white" />
         </a>
-        <a href="https://www.linkedin.com/in/jeffrey-pan-a352491b5/" aria-label="LinkedIn" className="text-gray-300 ml-4 hover:text-white">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" className="text-white" />
+        <a href="https://www.linkedin.com/in/jeffrey-pan-a352491b5/" aria-label="LinkedIn" className="ml-4 text-gray-300 hover:text-white">
+          <FontAwesomeIcon icon={faLinkedin} size="lg" className="text-white" />
         </a>
-      </div>
-
-      <div className="flex flex-row items-center text-gray-300">
-        <a href="#contact" aria-label="Email" className="text-gray-300 hover:text-white">
-          <MailIcon className="h-8 w-8 text-white" />
+        <a href="mailto:JeffreyPan95@gmail.com" aria-label="Email" className="ml-4 text-gray-300 hover:text-white">
+          <FontAwesomeIcon icon={faEnvelope} size="lg" className="text-white" />
         </a>
       </div>
     </header>

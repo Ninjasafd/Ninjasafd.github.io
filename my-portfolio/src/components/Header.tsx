1e43b778303f6649452994c7e481ddbe1382920d
Ninjasafd <JeffreyPan95@gmail.com>
2024-09-03 22:36:00 -0400

    Working header

--- my-portfolio/src/components/Header.tsx
+++ my-portfolio/src/components/Header.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import { MailIcon } from '@heroicons/react/solid';
 
 export default function Header() {
-  return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
-        <a href="https://github.com/Ninjasafd" aria-label="GitHub" className="text-gray-300 hover:text-white">
-          <FontAwesomeIcon icon={faGithub} size="lg" className="text-white" />
-        </a>
-        <a href="https://www.linkedin.com/in/jeffrey-pan-a352491b5/" aria-label="LinkedIn" className="ml-4 text-gray-300 hover:text-white">
-          <FontAwesomeIcon icon={faLinkedin} size="lg" className="text-white" />
-        </a>
-        <a href="mailto:JeffreyPan95@gmail.com" aria-label="Email" className="ml-4 text-gray-300 hover:text-white">
-          <FontAwesomeIcon icon={faEnvelope} size="lg" className="text-white" />
-        </a>
-      </div>
-    </header>
-  );
+    return (
+        <header className="sticky top-0 p-5 flex items-center justify-between max-w-7xl mx-auto z-20">
+            <div className="flex flex-row items-center">
+                <a href="https://github.com/Ninjasafd" aria-label="GitHub" className="text-gray-300 hover:text-white">
+                    <Image
+                        src="/icons/github_icon.png"
+                        alt="GitHub Icon"
+                        width={24}
+                        height={24}
+                        className="object-contain"
+                    />
+                </a>
+                <a href="https://www.linkedin.com/in/jeffrey-pan-a352491b5/" aria-label="LinkedIn" className="ml-4 text-gray-300 hover:text-white">
+                    <Image
+                        src="/icons/linkedin_icon.png"
+                        alt="LinkedIn Icon"
+                        width={24}
+                        height={24}
+                        className="object-contain"
+                    />
+                </a>
+            </div>
+            <div className="flex flex-row items-center">
+                <a href="mailto:JeffreyPan95@gmail.com" aria-label="Email" className="text-gray-300 hover:text-white">
+                    <MailIcon className="h-6 w-6 text-white" />
+                </a>
+            </div>
+        </header>
+    );
 }
